ad19d58813a6bbd49c5ecafc81c5fb66ab4bdde0
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-30 14:59:23 -0400

    multiples array function

---
+++ multiples.js
@@ -0,0 +1,16 @@
+// SWDV 620: Web Applications
+// Function that returns an array of the multiples of a number, num, with an optional length
+
+function multiplesArray(num, length = 10) {
+    let multiples = [];
+
+    for (let i = 1; i <= length; i++) {
+        let mult = num * i;
+        multiples.push(mult);
+    }
+
+    return multiples;
+}
+
+test = multiplesArray(4, 5);  // [ 4, 8, 12, 16, 20 ]
+console.log(test);
