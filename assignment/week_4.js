8c8e74fea1694658fa692cc2ec2c28a37fff590b
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-31 14:02:18 -0400

    JavaScript objects list

--- assignment/week_4.js
+++ assignment/week_4.js
@@ -5,6 +5,7 @@
 function login(username) {
     let message = 'Hello ' + username + '!';
     document.getElementById('welcomeMsg').innerText = message; 
+    createObjList();
 }
 
 function promptLogin() {
@@ -35,6 +36,7 @@ function parseColor(val) {
 }
 
 function setColor() {
+    // Changes background color on input
     let inputEle =  document.getElementById('color');
     let color = parseColor(inputEle.value);
 
@@ -45,9 +47,45 @@ function setColor() {
 }
 
 function reset() {
+    // Resets background on click
     document.body.style.backgroundColor = 'white';
     
     let inputEle =  document.getElementById('color');
     inputEle.style.color = 'black'; 
     inputEle.value = '';
+}
+
+/* JavaScript object list functions */
+
+function getRow(obj, example) {
+    // Returns a row containing an object name and example
+    let row = document.createElement('tr');
+
+    let obj_data = document.createElement('td');
+    obj_data.innerText = obj;
+    let example_data = document.createElement('td');
+    example_data.innerText = JSON.stringify(example);
+
+    row.appendChild(obj_data);
+    row.appendChild(example_data);
+
+    return row;
+}
+
+function addRow(obj, example) {
+    let table = document.getElementById('objectList');
+    table.appendChild(getRow(obj, example));
+}
+
+function createObjList() {
+    let objArr =
+    [
+        ['Array', [1, 2, 3, 4]], ['String', 'hello world!'], ['Number', 54.25],
+        ['Also Number', 54], ['Boolean', true], ['Object', {"value": 25}] 
+    ];
+
+    for (i in objArr) {
+        let rowArr = objArr[i];
+        addRow(rowArr[0], rowArr[1]);
+    }
 }
